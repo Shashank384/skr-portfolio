b46fbd50de54fe240085b8084810161c26f22aab
Shashank384 <shashank.mayur@gmail.com>
2020-08-28 13:55:15 +0530

    env var

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ let emailPwd;
   emailId = process.env.EMAIL;
   emailPwd = process.env.EMAIL_PWD;
 
-  
+  if(process.env.NODE_ENV === 'production') {
+    emailId = process.env.EMAIL;
+    emailPwd = process.env.EMAIL_PWD;
+    app.use(express.static('learning/build'))
+    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'learning', 'build', 'index.html')))
+
+  }
 
 app.post('/api/form', (req,res) => {
         console.log('Received')
@@ -56,11 +62,7 @@ app.post('/api/form', (req,res) => {
           });
     })
 
-    if(process.env.NODE_ENV === 'production') {
-      app.use(express.static('learning/build'))
-      app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'learning', 'build', 'index.html')))
-  
-    }
+   
 
 const PORT = process.env.PORT || 3001
 
